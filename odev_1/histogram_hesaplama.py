6e8e68470b143a7abf60eed24488694d3cf0f0d7
kker4m <keremmertizmir39@gmail.com>
2023-11-10 12:29:56 +0300

    Resimi griye cevirme eklendi

--- odev_1/histogram_hesaplama.py
+++ odev_1/histogram_hesaplama.py
@@ -2,18 +2,21 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Resim okunur ve griye cevirilir
 image_path = "ornek_resim.png"
 image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-height, width, _ = image.shape
+#Boy ve enlem alinir
+height, width = gray_image.shape
 
-
+#Histogramlar icin bos array olusturulur
 hist = np.zeros((256,))
 
-
+#Her pixel gezilir, bir arttirilarak histogram arrayinde dogru konuma kayit edilir
 for y in range(height):
     for x in range(width):
-        pixel_value = image[y, x, 0]
+        pixel_value = gray_image[y, x]
         hist[pixel_value] += 1
 
 plt.plot(hist, color='gray')
