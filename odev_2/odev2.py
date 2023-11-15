c9113ece7f13dd4864eb8c81fcb9458ab115d055
kker4m <keremmertizmir39@gmail.com>
2023-11-15 14:43:12 +0300

    Ornek Gorsel eklendi, kamera ile okuma eklendi

--- odev_2/odev2.py
+++ odev_2/odev2.py
@@ -1,13 +1,12 @@
 import cv2
 import numpy as np
 
-#cap = cv2.VideoCapture(0)
-image = cv2.imread('maxresdefault.jpeg')
+cap = cv2.VideoCapture(0)
+
 while True:
-    #ret, frame = cap.read()
+    ret, frame = cap.read()
 
-    hsv_gorsel = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
+    hsv_gorsel = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     low_kirmizi = np.array([0, 100, 100])
     up_kirmizi = np.array([10, 255, 255])
@@ -15,13 +14,10 @@ while True:
     aralik = cv2.inRange(hsv_gorsel, low_kirmizi, up_kirmizi)
     ters_aralik = 255 - aralik
 
-    #result = cv2.bitwise_and(frame, frame, mask=aralik)
-    result = cv2.bitwise_and(image, image, mask=aralik)
+    result = cv2.bitwise_and(frame, frame, mask=aralik)
 
-
-    #cv2.imshow('Original', frame)
-    cv2.imshow('Original', image)
-    cv2.imshow('Result', result)
+    cv2.imshow('Kamera goruntusu', frame)
+    cv2.imshow('Cikti', result)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
